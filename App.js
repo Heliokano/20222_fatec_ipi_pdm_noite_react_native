4b6e9b4ed6ae899adfe06bd5c468fbe454cadbb8
Helio Okano <helio_okano2@yahoo.com.br>
2022-11-04 21:31:48 -0300

    aplicacao do zero

--- App.js
+++ App.js
@@ -1,34 +1,16 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
-import {useState} from 'react'
+import { StyleSheet, View } from 'react-native';
 
-//hooks do React 
-//https://pt-br.reactjs.org/docs/hooks-overview.html
 
 export default function App() {
-  const [contador, setContador] = useState(0)
-
-  const incrementar = () => {
-    setContador(contador + 1)
-  }
-  // const [texto, setTexto] = useState('esse valor com vetor')
-  // const texto = vetor[0]
-  // const setTexto = vetor[1]
+  
   return (
-    <View style={styles.container}>
-      <Text>{contador}</Text>
-      <Button
-        title = 'Incrementar'
-        onPress = {incrementar}
-        />
+    <View>
+      
     </View>
   );
-}
 
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
+  
 });
